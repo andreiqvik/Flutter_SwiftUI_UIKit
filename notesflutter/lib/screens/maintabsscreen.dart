4968c5b96d44b555945f23b7b13ca477eb3e35ea
Andrei Sadovnicov <andrei.sadovnicov@qvik.fi>
2022-09-20 16:48:21 +0300

    Add main tabs screen in NotesFlutter

---
+++ notesflutter/lib/screens/maintabsscreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+
+class CupertinoTabBarApp extends StatelessWidget {
+  const CupertinoTabBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoTabBarExample(),
+    );
+  }
+}
+
+class CupertinoTabBarExample extends StatelessWidget {
+  const CupertinoTabBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.list_dash),
+            label: 'Notes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart_fill),
+            label: 'Favorite notes',
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        return CupertinoTabView(
+          builder: (BuildContext context) {
+            return Center(
+              child: Text('Content of tab $index'),
+            );
+          },
+        );
+      },
+    );
+  }
+}
