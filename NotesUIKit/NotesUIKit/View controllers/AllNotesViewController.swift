35d887307f74de5fa3e8c64ff9dd643660272b08
Andrei Sadovnicov <andrei.sadovnicov@qvik.fi>
2022-09-20 09:29:17 +0300

    Add view controller files and scenes

---
+++ NotesUIKit/NotesUIKit/View controllers/AllNotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AllNotesViewController.swift
+//  NotesUIKit
+//
+//  Created by Andrei Sadovnicov on 20.9.2022.
+//
+
+import UIKit
+
+class AllNotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
