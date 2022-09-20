938169efff67a39fffcd05a7cae796ee505602ee
Andrei Sadovnicov <andrei.sadovnicov@qvik.fi>
2022-09-20 12:30:30 +0300

    Add NotesSwiftUI app

---
+++ NotesSwiftUI/NotesSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NotesSwiftUI
+//
+//  Created by Andrei Sadovnicov on 20.9.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesSwiftUI/NotesSwiftUI/NotesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesSwiftUIApp.swift
+//  NotesSwiftUI
+//
+//  Created by Andrei Sadovnicov on 20.9.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
