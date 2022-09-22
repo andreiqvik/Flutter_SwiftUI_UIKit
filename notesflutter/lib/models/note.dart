cd03c4f4daf483c0a372142970c7c0b55c620064
Andrei Sadovnicov <andrei.sadovnicov@qvik.fi>
2022-09-22 15:21:40 +0300

    Set title and subtitle in NotesFlutter

--- notesflutter/lib/models/note.dart
+++ notesflutter/lib/models/note.dart
@@ -1,12 +1,25 @@
+import 'package:collection/collection.dart';
 import 'package:isar/isar.dart';
 part 'note.g.dart';
 
 @collection
 class Note {
+  // PROPERTIES
   Id id = Isar.autoIncrement;
   String? content;
   String? title;
   String? subtitle;
   bool isFavorite = false;
   DateTime lastUpdate = DateTime.now();
+
+  // METHODS
+  void setTitleAndSubtitle() {
+    final rows = content?.split("\n");
+    if (rows != null) {
+      title = rows.firstOrNull;
+      if (rows.length > 1) {
+        subtitle = rows[1];
+      }
+    }
+  }
 }
